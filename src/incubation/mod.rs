471f39fe51334d746afb0c770b8109ba11d61aa6
panicbit <panicbit.dev@gmail.com>
2015-12-28 06:44:09 +0100

    Add incubation::swap_tuple

--- src/incubation/mod.rs
+++ src/incubation/mod.rs
@@ -2,3 +2,6 @@
 //! which are being matured and tested for permanent inclusion.
 //! Items in this module should be considered unstable and thus
 //! may change or get removed completely at any time.
+
+pub mod swap_tuple;
+pub use self::swap_tuple::SwapTupleExt;
---
+++ src/incubation/swap_tuple.rs
@@ -0,0 +1,38 @@
+
+pub trait SwapTupleExt {
+    type Result;
+    fn swap(self) -> Self::Result;
+}
+
+impl <T, U> SwapTupleExt for (T, U) {
+    type Result = (U, T);
+    fn swap(self) -> Self::Result {
+        (self.1, self.0)
+    }
+}
+
+impl <'a, T, U> SwapTupleExt for &'a (T, U) {
+    type Result = (&'a U, &'a T);
+    fn swap(self) -> Self::Result {
+        (&self.1, &self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SwapTupleExt;
+
+    #[test]
+    fn swap_tuple_value() {
+        let tuple: (&str, i32) = ("foo", 42);
+        let swapped: (i32, &str) = tuple.swap();
+        assert_eq!(swapped, (42, "foo"));
+    }
+
+    #[test]
+    fn swap_tuple_ref() {
+        let tuple: &(&str, i32) = &("foo", 42);
+        let swapped: (&i32, &&str) = tuple.swap();
+        assert_eq!(swapped, (&42, &"foo"));
+    }
+}
