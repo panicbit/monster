5061e510fabf314d872f126f6aa4ec9afb5c90e9
panicbit <panicbit.dev@gmail.com>
2015-12-28 06:43:51 +0100

    Add incubation module

---
+++ src/incubation/mod.rs
@@ -0,0 +1,4 @@
+//! This module includes new utilities
+//! which are being matured and tested for permanent inclusion.
+//! Items in this module should be considered unstable and thus
+//! may change or get removed completely at any time.
