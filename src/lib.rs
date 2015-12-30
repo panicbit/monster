7c54ea94cd3d9d9d1cdd743abd5819610934ceb2
panicbit <panicbit.dev@gmail.com>
2015-12-30 04:10:18 +0100

    Set html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 #![cfg_attr(feature="unstable",feature(unboxed_closures, fn_traits,stmt_expr_attributes))]
+#![doc(html_root_url = "https://panicbit.github.io/monster/monster/")]
 
 pub mod incubation;
