1de56777003444e504972cffd7bc104350cf72d7
panicbit <panicbit.dev@gmail.com>
2016-01-08 20:34:05 +0100

    Add incubation::global_init_cleanup

---
+++ src/incubation/global_init_cleanup.rs
@@ -0,0 +1,78 @@
+//!
+//! # Example
+//! ```
+//! #[macro_use]
+//! extern crate lazy_static;
+//! extern crate monster;
+//! 
+//! use monster::incubation::global_init_cleanup::{InitCleanup, GlobalInitHandle, InitHandle};
+//! use std::sync::Arc;
+//! 
+//! lazy_static! {
+//!     static ref INIT_HANDLE: GlobalInitHandle<MyLib> = InitHandle::new_global();
+//! }
+//! 
+//! struct MyLib {
+//!     init_handle: Arc<InitHandle<MyLib>>
+//! }
+//! 
+//! impl MyLib {
+//!     fn new() -> MyLib {
+//!         let init_handle = InitHandle::from_global(&INIT_HANDLE);
+//! 
+//!         MyLib {
+//!             init_handle: init_handle
+//!         }
+//!     }
+//! }
+//! 
+//! impl InitCleanup for MyLib {
+//!     fn init() { println!("LIB_INIT()"); }
+//!     fn cleanup() { println!("LIB_CLEANUP()"); }
+//! }
+//! 
+//! fn main() {
+//!     let l1 = MyLib::new();
+//!     let l2 = MyLib::new();
+//!     let l3 = MyLib::new();
+//! }
+//! ```
+
+use std::sync::{Arc,Weak,Mutex};
+use std::marker;
+
+pub trait InitCleanup {
+    fn init();
+    fn cleanup();
+}
+
+pub struct InitHandle<T: InitCleanup>(marker::PhantomData<T>);
+
+impl <T: InitCleanup> InitHandle<T> {
+    pub fn new_global() -> GlobalInitHandle<T> {
+        Mutex::new(None)
+    }
+
+    pub fn from_global(handle: &Mutex<Option<Weak<InitHandle<T>>>>) -> Arc<InitHandle<T>> {
+        let mut handle = handle.lock().unwrap();
+        handle.as_ref()
+        .and_then(Weak::upgrade)
+        .unwrap_or_else(|| {
+            let new_handle = Arc::new(InitHandle(marker::PhantomData));
+            *handle = Some(Arc::downgrade(&new_handle));
+
+            T::init();
+
+            new_handle
+        })
+    }
+}
+
+impl <T: InitCleanup> Drop for InitHandle<T> {
+    fn drop(&mut self) {
+        T::cleanup()
+    }
+}
+
+
+pub type GlobalInitHandle<T> = Mutex<Option<Weak<InitHandle<T>>>>;
--- src/incubation/mod.rs
+++ src/incubation/mod.rs
@@ -46,3 +46,6 @@ pub use self::tuple_iter::TupleIterExt;
 
 pub mod ref_eq;
 pub use self::ref_eq::RefEq;
+
+pub mod global_init_cleanup;
+pub use self::global_init_cleanup::{InitCleanup, GlobalInitHandle, InitHandle};
