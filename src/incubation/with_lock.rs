271b0672be1e12a230d2b22d828310533d868635
panicbit <panicbit.dev@gmail.com>
2016-01-15 13:18:26 +0100

    Add with_lock documentation

--- src/incubation/with_lock.rs
+++ src/incubation/with_lock.rs
@@ -1,6 +1,19 @@
 use std::sync::Mutex;
 
 pub trait WithLock<T> {
+    /// Conveniently access mutexes.
+    ///
+    /// # Example
+    /// ```
+    /// use std::sync::Mutex;
+    /// use monster::incubation::WithLock;
+    ///
+    /// let n = Mutex::new(123);
+    ///
+    /// n.with_lock(|n| {
+    ///     *n += 654
+    /// });
+    /// ```
     fn with_lock<F: Fn(&mut T) -> R, R>(&self, f: F) -> R;
 }
 
